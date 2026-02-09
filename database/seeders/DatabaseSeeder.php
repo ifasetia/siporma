7b96652927da345ed4974b8fbaedd63d596531b8
Dindaafrilia <afriliadindaaaaa@gmail.com>
2026-02-09 11:49:24 +0700

    tambah profile seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             KampusSeeder::class,
             PekerjaanSeeder::class,
+            ProfileSeeder::class,
         ]);
     }
 }
